e30026a6308c621cd920ef52d0cc78fcbca83cec
Md Shah Alam <mdshahalam8283@gmail.com>
2024-04-08 01:54:31 +0600

    Bank App Done

---
+++ bank_app/main.js
@@ -0,0 +1,36 @@
+
+
+class bankAccount {
+    ownerName = "Md Shah Alam";
+    accountNo = "37159912";
+    balance = 0;
+
+    diposite(amount) {
+        this.balance += amount;
+    }
+
+    widthrow(amount) {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+            this.getBalance();
+        } else {
+            console.log("Insufficient funds");
+        }
+    }
+    getBalance() {
+        return this.balance;
+    }
+
+    displayAccountInfo() {
+        return (
+            `Account Number : ${this.accountNo}
+Owner Name : ${this.ownerName}
+Balance : ${this.getBalance()}
+`);
+    }
+}
+const account = new bankAccount();
+account.diposite(1000);
+account.widthrow(400);
+// console.log(account.getBalance());
+console.log(account.displayAccountInfo());
